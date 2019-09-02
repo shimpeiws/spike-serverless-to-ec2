f6bae2328277fac2299e7bea28ac0fea5ff0eae3
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-09-03 05:16:35 +0900

    Scheduled trigger

--- handler.ts
+++ handler.ts
@@ -1,11 +1,11 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 
-export const hello: APIGatewayProxyHandler = async (event, context) => {
+export const hello: APIGatewayProxyHandler = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }),
+      input: event
+    })
   };
-}
+};
