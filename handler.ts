078fa1e622e3da2ce53c793708e7d18ef8cd288e
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-09-03 15:42:33 +0900

    Implement start and stop functions

--- handler.ts
+++ handler.ts
@@ -1,11 +1,57 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { EC2 } from 'aws-sdk';
+
+const ec2Client = () => {
+  return new EC2({ apiVersion: '2016-11-15' });
+};
+
+const instanceIds = () => {
+  return ['i-YOUR-INSTANCE-ID'];
+};
 
 export const hello: APIGatewayProxyHandler = async event => {
+  console.info('Hello');
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
       input: event
+    })
+  };
+};
+
+export const start: APIGatewayProxyHandler = async _ => {
+  console.info('EC2 Start');
+  try {
+    const res = await ec2Client()
+      .startInstances({ InstanceIds: instanceIds() })
+      .promise();
+    console.info('res', res);
+  } catch (error) {
+    console.error('error startInstances: ', error);
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'EC2 Start finished'
+    })
+  };
+};
+
+export const stop: APIGatewayProxyHandler = async _ => {
+  console.info('EC2 Stop');
+  try {
+    const res = await ec2Client()
+      .stopInstances({ InstanceIds: instanceIds() })
+      .promise();
+    console.info('res', res);
+  } catch (error) {
+    console.error('error stopInstances: ', error);
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'EC2 Stop finished'
     })
   };
 };
